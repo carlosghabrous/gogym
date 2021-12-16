23a1e4e5e14855f3187682150ce3384c7cea0a6e
carlosghabrous <carlos.ghabrous@gmail.com>
2021-12-16 16:13:31 +0100

    Change printed messages

--- pkg/gym/bluebook/ch01.go
+++ pkg/gym/bluebook/ch01.go
@@ -5,11 +5,11 @@ import (
 )
 
 func Exercise01(args ...interface{}) error {
-	fmt.Println("Exercise 01 blue book, chapter 01")
+	fmt.Println("Executing exercise 01 blue book, chapter 01")
 	return nil
 }
 
 func Exercise02(args ...interface{}) error {
-	fmt.Println("Exercise 02 blue book, chapter 01")
+	fmt.Println("Executing exercise 02 blue book, chapter 01")
 	return nil
 }
