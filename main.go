f85dd9026a6659601613bacb1021df3d27c0cc5f
carlosghabrous <carlos.ghabrous@gmail.com>
2022-01-12 22:38:44 +0100

    Add next exercise

--- main.go
+++ main.go
@@ -13,15 +13,22 @@ func init() {
 }
 
 func initBlueBook() {
-	e01 := &menu.Exercise{MD: menu.MetaData{Id: "Statistics", Description: "Chapter02, Ex01"}, Runner: bluebook.Exercise01}
-	e02 := &menu.Exercise{MD: menu.MetaData{Id: "Quadratic", Description: "Chapter02, Ex02"}, Runner: bluebook.Exercise02}
-	blue01 := &menu.Section{MD: menu.MetaData{Id: "Chapter02", Description: "Blue book's chapter 02"}}
+	// Chapter 02
+	e01c02 := &menu.Exercise{MD: menu.MetaData{Id: "Statistics", Description: "Chapter02, Ex01"}, Runner: bluebook.Exercise01C02}
+	e02c02 := &menu.Exercise{MD: menu.MetaData{Id: "Quadratic", Description: "Chapter02, Ex02"}, Runner: bluebook.Exercise02C02}
+	blue02 := &menu.Section{MD: menu.MetaData{Id: "Chapter02", Description: "Ch 02: Booleans and numbers"}}
 
-	blue01.Attach(e01)
-	blue01.Attach(e02)
+	blue02.Attach(e01c02)
+	blue02.Attach(e02c02)
+
+	// Chapter 03
+	e01c03 := &menu.Exercise{MD: menu.MetaData{Id: "M3u2pls", Description: "Chapter03, Ex01"}, Runner: bluebook.Exercise01C03}
+	blue03 := &menu.Section{MD: menu.MetaData{Id: "Chapter03", Description: "Ch 03: Strings"}}
+	blue03.Attach(e01c03)
 
 	blueBook := &menu.Section{MD: menu.MetaData{Id: "Programming in Go book", Description: "Programming in Go book's exercises"}}
-	blueBook.Attach(blue01)
+	blueBook.Attach(blue02)
+	blueBook.Attach(blue03)
 
 	menu.Add("Programming in Go book", blueBook)
 }
